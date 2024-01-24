27cfe70af665cddcbc9bbbd28ad535d5adc0314c
Brandon <github@briley.me>
2024-01-23 19:28:20 -0700

    Added Scripts

---
+++ AI/Scripts/AgentBehaviour.cs
@@ -0,0 +1,20 @@
+#From the Repo of Anthony romrell
+
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
---
+++ AI/Scripts/SimplerAI.cs
@@ -0,0 +1,21 @@
+﻿#From the repo of Anthony Romrell
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimplerAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform playerTransformObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerTransformObj.position);
+    }
+}

904ca5e9b428dc31046ed128131645ee93824bea
Brandon <github@briley.me>
2024-01-24 14:37:01 -0700

    File Organizaiton

---
+++ UI/TextBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehaviour : MonoBehaviour
+{
+    public Text label;
+    public FloatData dataObj;
+
+    private void Start()
+    {
+        label = GetComponent<Text>();
+        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}

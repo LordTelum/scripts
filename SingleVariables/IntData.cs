946421c5a8859847f0c1d3e9adeb422d16852ed3
Brandon <github@briley.me>
2024-01-26 20:02:11 -0700

    Added Scripts

---
+++ SingleVariables/IntData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public void UpdateValue(int num)
+    {
+        value += num;
+    }
+}
---
+++ Vector3Data/Vector3Data.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Single Variables/Vector3Data")]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 value;
+
+    public void UpdateValue(Transform obj)
+    {
+        value = obj.position;
+    }
+    public void UpdateTransfrom(Transform obj)
+    {
+        value = obj.position;
+    }
+}

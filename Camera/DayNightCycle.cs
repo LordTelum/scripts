97bb75ec603a091063f74581a798e230a2269293
Brandon <github@briley.me>
2024-01-29 09:25:42 -0700

    Day and night cycle script added

---
+++ Camera/DayNightCycle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DayNightCycle : MonoBehaviour
+{
+    //duration of the day in seconds
+    public float dayLength = 120f;
+    public UnityEvent morningEvent, nightEvent;
+    
+    private float rotationSpeed;
+     //0 is midnight, 0.5 is noon
+    private float timeOfDay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = 360f / dayLength;
+    }
+    
+    void Update()
+    {
+        //rotate the sun around the y axis
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        
+        //update the time of day
+        timeOfDay += Time.deltaTime / dayLength;
+        
+        //if it is morning
+        if (timeOfDay > 0.25f && timeOfDay < 0.26f)
+        {
+            morningEvent.Invoke();
+        }
+        
+        //if it is night
+        if (timeOfDay > 0.75f && timeOfDay < 0.76f)
+        {
+            nightEvent.Invoke();
+        }
+        
+        //reset the time of day
+        if (timeOfDay > 1f)
+        {
+            timeOfDay -= 1f;
+        }
+    }
+}

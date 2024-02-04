8d636aecbb04c35c33ea709ae4610ac67b60d6ac
Brandon <github@briley.me>
2024-02-03 17:34:06 -0700

    ColorID update

--- ColorID.cs
+++ ColorID.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorID : MonoBehaviour
+[CreateAssetMenu]
+public class ColorID : ID
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Color value = Color.red;
 }

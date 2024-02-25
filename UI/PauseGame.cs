d05f4bbf2a707681354a527d5c9031b63d5914af
Brandon <github@briley.me>
2024-02-25 14:38:11 -0700

    Pause menu Script

---
+++ UI/PauseGame.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void Home()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
